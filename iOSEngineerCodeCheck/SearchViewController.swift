5e529724d692ac190e98fa25932c2bda8f46640f
yumi kanebayashi <bon.kyumi@gmail.com>
2020-12-23 20:13:26 +0900

    [修正]SearchControllerへ変更 / UITestの追加 searchBarからsearchControllerに変更しました. SearchViewControllerをUITableViewControllerからUIViewControllerに変更しました. UITestを導入しました.

--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -5,9 +5,10 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController, UISearchBarDelegate {
-
-    @IBOutlet weak var searchBar: UISearchBar!
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var searchController: UISearchController!
     
     var repositories: [[String: Any]] = []
     
@@ -19,71 +20,97 @@ class SearchViewController: UITableViewController, UISearchBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchBar.text = "GitHubのリポジトリを検索できるよー"
-        searchBar.delegate = self
+        setupSearchController()
     }
+    
+    private func setupSearchController() {
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.placeholder = "リポジトリ名を入力"
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        navigationItem.title = "GitHub リポジトリ検索"
+        if #available(iOS 11.0, *) {
+            // UISearchControllerをUINavigationItemのsearchControllerプロパティにセットする。
+            navigationItem.searchController = searchController
+            
+            // trueだとスクロールした時にSearchBarを隠す（デフォルトはtrue）
+            // falseだとスクロール位置に関係なく常にSearchBarが表示される
+            navigationItem.hidesSearchBarWhenScrolling = true
+        } else {
+            tableView.tableHeaderView = searchController.searchBar
+        }
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        searchController.searchBar.delegate = self
+    }
+
+}
+
+// MARK: - SearchBarDelegate
+extension SearchViewController: UISearchBarDelegate {
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        // 初期化
-        searchBar.text = ""
         return true
     }
-    
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         task?.cancel()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
+
         searchWord = searchBar.text!
-        
+
         if searchWord.count != 0 {
             url = "https://api.github.com/search/repositories?q=\(searchWord!)"
             task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
                 if let object = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = object["items"] as? [[String: Any]] {
-                    self.repositories = items
+                        self.repositories = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
                     }
                 }
             }
-        // リストの更新
-        task?.resume()
+            // リストの更新
+            task?.resume()
         }
-        
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if segue.identifier == "showDetail" {
-            let detailViewController = segue.destination as! DetailViewController
-            detailViewController.searchViewController = self
-        }
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
+
+// MARK: - TableViewDataSource
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repositories.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let repository = repositories[indexPath.row]
         cell.textLabel?.text = repository["full_name"] as? String ?? ""
         cell.detailTextLabel?.text = repository["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+}
+
+// MARK: - TableViewDelegate
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
         index = indexPath.row
         performSegue(withIdentifier: "showDetail", sender: self)
-        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.searchViewController = self
+        }
+    }
 }
--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -11,16 +11,12 @@ import XCTest
 class iOSEngineerCodeCheckUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() throws {
@@ -28,13 +24,26 @@ class iOSEngineerCodeCheckUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.searchFields["リポジトリ名を入力"].tap()
+        app.typeText("tetris")
+        app/*@START_MENU_TOKEN@*/.buttons["Search"]/*[[".keyboards",".buttons[\"検索\"]",".buttons[\"Search\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(3)
+        
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["chvin/react-tetris"]/*[[".cells.staticTexts[\"chvin\/react-tetris\"]",".staticTexts[\"chvin\/react-tetris\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(3)
+        
+        app.staticTexts["chvin/react-tetris"].tap()
+        app.staticTexts["Written in JavaScript"].tap()
+        app.staticTexts["6333 stars"].tap()
+        app.staticTexts["6333 watchers"].tap()
+        app.staticTexts["1206 forks"].tap()
+        app.staticTexts["2 open issues"].tap()
+        app.navigationBars["iOSEngineerCodeCheck.DetailView"].buttons["GitHub リポジトリ検索"].tap()
+                
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }

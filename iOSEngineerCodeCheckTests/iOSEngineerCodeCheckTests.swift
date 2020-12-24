e4816cbd3c8c71808f9de6d0e9111b7bda0c40f4
yumi kanebayashi <bon.kyumi@gmail.com>
2020-12-24 16:14:22 +0900

    [追加]Unit testの追加 簡単な形式でapiRequest関数のテストにチャレンジしました.

--- iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
+++ iOSEngineerCodeCheckTests/iOSEngineerCodeCheckTests.swift
@@ -10,25 +10,26 @@ import XCTest
 @testable import iOSEngineerCodeCheck
 
 class iOSEngineerCodeCheckTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+//    func testExample() throws {
+//    }
+    
+    // apiRequest関数に正しいurlとqueryを渡すとレスポンスが返り値が格納されるのを確認
+    func testApiRequest() {
+        let vc = SearchViewController()
+        vc.apiRequest(url: "https://api.github.com/search/repositories", query: [URLQueryItem(name: "q", value: "tetris")])
+        XCTAssertNotNil(vc.repositories)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+//    func testPerformanceExample() throws {
+//        self.measure {
+//
+//        }
+//    }
 }
